b83a6eaf58bd3df370ca1b7878f7ec26a7a3b86b
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-01 00:09:19 +0300

    Add files via upload

---
+++ ArcanoidNV/ArcanoidNV/Ball.cs
@@ -0,0 +1,13 @@
+﻿namespace ArcanoidNV
+{
+    internal class Ball
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int speedX { get; set; }
+        public int speedY { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+    }
+}
