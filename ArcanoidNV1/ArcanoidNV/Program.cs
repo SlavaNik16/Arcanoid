941d78a83f451dc99384d745c266aa34cb138041
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-16 21:33:57 +0300

    Add files via upload

---
+++ ArcanoidNV1/ArcanoidNV/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArcanoidNV
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            OsnovaForm osn = new OsnovaForm();
+            MenuForm menu = new MenuForm();
+            menu.Show();
+            osn.Opacity = 0;
+            Application.Run(osn);
+
+
+        }
+    }
+}
